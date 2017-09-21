b1f31196e003448b4a4ea3632efd4ab30e64fca0
Gaetan <gaetan.raynaud@ensea.fr>
2017-09-21 17:43:20 +0200

    Correct bug when no arg in command line

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -16,14 +16,14 @@ using namespace state;
 
 int main(int argc,char* argv[]) 
 {
-
-    if (string(argv[1]) == "hello") {
-        cout << "Bonjour le monde!" << endl;
-    } else {
-        cout << "Commande inconnue" << endl;
-        cout << "Liste des commandes : " << endl;
-        cout << "hello : Affiche un message de bienvenue" << endl; 
+    if( argc > 1) {
+        if (string(argv[1]) == "hello") {
+            cout << "Bonjour le monde!" << endl;
+        } else {
+            cout << "Commande inconnue" << endl;
+            cout << "Liste des commandes : " << endl;
+            cout << "hello : Affiche un message de bienvenue" << endl; 
+        }
     }
-    
     return 0;
 }
