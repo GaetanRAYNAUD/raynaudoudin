7380d9913c49439256d29b36a4bed1d8cbeae2be
Gaetan <gaetan.raynaud@ensea.fr>
2017-10-03 12:02:47 +0200

    Add tests for state and team

---
+++ src/client/state.hpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   state.hpp
+ * Author: gaetan
+ *
+ * Created on 3 octobre 2017, 10:37
+ */
+
+#ifndef STATE_HPP
+#define STATE_HPP
+
+#include "../shared/state.h"
+
+namespace state {
+    
+    bool unitTest_shouldCreateState();
+    bool unitTest_shouldAddTeamToBoard();
+    bool unitTest_shouldAddTerrainToBoard();
+    bool unitTest_shouldAddUnitToBoard();
+    bool unitTest_shouldAddWeaponToUnit();
+    void unitTest();
+    
+}
+
+#endif /* STATE_HPP */
+
