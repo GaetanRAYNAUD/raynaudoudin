15b74090191371c9a372ba675f869265e46ef088
mathis <mathis.oudin@ensea.fr>
2017-10-05 19:09:20 +0200

    Add unit test unitTest_shouldAddUnitToBoard()

--- src/client/state.cpp
+++ src/client/state.cpp
@@ -17,6 +17,7 @@ namespace state {
         int teamsSize = state->getBoard().getTeams().size();
         
         std::cout << "  Il y a actuellement " << teamsSize << " équipe(s) dans le jeu" << std::endl;
+        
         std::cout << "  Ajout d'une équipe au jeu" << std::endl;
         
         Team* team = new Team(1);
@@ -32,10 +33,11 @@ namespace state {
         int terrainSize = state->getBoard().getTerrains().size();
         
         std::cout << "  Il y a actuellement " << terrainSize << " terrain(s) dans le jeu" << std::endl;
+        
         std::cout << "  Ajout d'un terrain de type HOUSE au jeu" << std::endl;
         
-        House* house = new House(1, 1, 1);
-        state->getBoard().addTerrain(house);
+        Terrain* terrain = new House(1, 1, 1);
+        state->getBoard().addTerrain(terrain);
         
         terrainSize = state->getBoard().getTerrains().size();
         std::cout << "  Il y a maintenant " << terrainSize << " terrain(s) dans le jeu" << std::endl;
@@ -53,8 +55,8 @@ namespace state {
         
         std::cout << "  Ajout d'un terrain de type CASTLE au jeu" << std::endl;
         
-        Castle* castle = new Castle(2, 1, 1);
-        state->getBoard().addTerrain(castle);
+        terrain = new Castle(2, 1, 1);
+        state->getBoard().addTerrain(terrain);
         
         terrainSize = state->getBoard().getTerrains().size();
         std::cout << "  Il y a maintenant " << terrainSize << " terrain(s) dans le jeu" << std::endl;
@@ -73,14 +75,87 @@ namespace state {
     }
 
     bool unitTest_shouldAddUnitToBoard() {
-        return false;    
+        int unitSize = state->getBoard().getUnits().size();
+        
+        std::cout << "  Il y a actuellement " << unitSize << " unité(s) dans le jeu" << std::endl;
+        
+        std::cout << "  Ajout d'une unité de type SWORDMAN au jeu" << std::endl;
+        
+        Unit* unit = new Swordman(1, 1, 1);
+        state->getBoard().addUnit(unit);
+        
+        unitSize = state->getBoard().getUnits().size();
+        std::cout << "  Il y a maintenant " << unitSize << " unité(s) dans le jeu" << std::endl;
+        
+        if(unitSize != 1) {
+            return false;
+        }
+        
+        UnitTypeId unitType = state->getBoard().getUnits().at(1)->getTypeId();
+        if(unitType == UnitTypeId::SWORDMAN) {
+            std::cout << "  L'unité ajouté est bien de type SWORDMAN" << std::endl;
+        } else {
+            return false;
+        }
+        
+        int weaponSize = state->getBoard().getUnits().at(1)->getWeapons().size();
+        std::cout << "  L'unité possède " << weaponSize << " arme(s)" << std::endl;
+        
+        if(weaponSize != 1) {
+            return false;
+        }        
+        
+        WeaponTypeId weaponType = state->getBoard().getUnits().at(1)->getWeapons().find(WeaponTypeId::SWORD)->second->getTypeId();
+        if(weaponType == WeaponTypeId::SWORD) {
+            std::cout << "  L'unité possède bien une arme de type SWORD" << std::endl;
+        } else {
+            return false;
+        }
+        
+        std::cout << "  Ajout d'une unité de type LEADER au jeu" << std::endl;
+        
+        unit = new Leader(2, 1, 1);
+        state->getBoard().addUnit(unit);
+        
+        unitSize = state->getBoard().getUnits().size();
+        std::cout << "  Il y a maintenant " << unitSize << " unité(s) dans le jeu" << std::endl;
+                
+        if(unitSize != 2) {
+            return false;
+        }
+        
+        unitType = state->getBoard().getUnits().at(2)->getTypeId();
+        if(unitType == UnitTypeId::LEADER) {
+            std::cout << "  L'unité ajouté est bien de type LEADER" << std::endl;
+        } else {
+            return false;
+        }
+        
+        weaponSize = state->getBoard().getUnits().at(2)->getWeapons().size();
+        std::cout << "  L'unité possède " << weaponSize << " arme(s)" << std::endl;
+        
+        if(weaponSize != 2) {
+            return false;
+        }     
+        
+        weaponType = state->getBoard().getUnits().at(2)->getWeapons().find(WeaponTypeId::SWORD)->second->getTypeId();
+        if(weaponType == WeaponTypeId::SWORD) {
+            std::cout << "  L'unité possède bien une arme de type SWORD" << std::endl;
+        } else {
+            return false;
+        }
+        
+        weaponType = state->getBoard().getUnits().at(2)->getWeapons().find(WeaponTypeId::BOW)->second->getTypeId();
+        if(weaponType == WeaponTypeId::BOW) {
+            std::cout << "  L'unité possède bien une arme de type BOW" << std::endl;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    bool unitTest_shouldAddWeaponToUnit() {
-        return false;    
-    }
-
-    void unitTest() {    
+    void unitTest() {
+        std::cout << "***************************************************" << std::endl;
         std::cout << "Tentative de création d'un état" << std::endl;
 
         if(unitTest_shouldCreatState()) {
@@ -88,6 +163,7 @@ namespace state {
         } else {
             std::cout << "ERREUR : Etat non créé !" << std::endl;
         }
+        std::cout << "***************************************************" << std::endl;
         
         std::cout << "Tentative d'ajout d'une équipe au jeu" << std::endl;
         
@@ -96,14 +172,25 @@ namespace state {
         } else {
             std::cout << "ERREUR : Equipe non ajoutée au jeu !" << std::endl;
         }
+        std::cout << "***************************************************" << std::endl;
 
-        std::cout << "Tentative d'ajout d'un terrain au jeu" << std::endl;
+        std::cout << "Tentative d'ajout de terrains au jeu" << std::endl;
         
         if(unitTest_shouldAddTerrainToBoard()) {
             std::cout << "Terrains correctement ajouté au jeu" << std::endl;
         } else {
             std::cout << "ERREUR : L'ajout de terrain au jeu à planté !" << std::endl;
         }
+        std::cout << "***************************************************" << std::endl;
+        
+        std::cout << "Tentative d'ajout d'unités au jeu" << std::endl;
+        
+        if(unitTest_shouldAddUnitToBoard()) {
+            std::cout << "Unités correctement ajouté au jeu" << std::endl;
+        } else {
+            std::cout << "ERREUR : L'ajout d'unité au jeu à planté !" << std::endl;
+        }
+        std::cout << "***************************************************" << std::endl;
         
     }
 
--- src/client/state.hpp
+++ src/client/state.hpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   state.hpp
- * Author: gaetan
- *
- * Created on 3 octobre 2017, 10:37
- */
-
 #ifndef STATE_HPP
 #define STATE_HPP
 
@@ -22,7 +9,6 @@ namespace state {
     bool unitTest_shouldAddTeamToBoard();
     bool unitTest_shouldAddTerrainToBoard();
     bool unitTest_shouldAddUnitToBoard();
-    bool unitTest_shouldAddWeaponToUnit();
     void unitTest();
     
 }
