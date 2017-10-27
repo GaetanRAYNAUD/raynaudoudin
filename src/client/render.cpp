0fe73be041da5ab4f84e8bb521791c4e64b21894
Gaetan <gaetan.raynaud@ensea.fr>
2017-10-27 17:00:36 +0200

    Add limit to 30fps and refresh units and menus surface in loop

--- src/client/render.cpp
+++ src/client/render.cpp
@@ -6,42 +6,40 @@ namespace render {
     
     void renderTest() {
         int windowWidth = 1152;
-            int windowHeight = 576;
-            sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "BfW");
+        int windowHeight = 576;
+        sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "BfW");
+        window.setFramerateLimit(30);
             
-            State* state = new State();
-            state->getBoard().loadTerrainsFromFile("res/map.txt");
+        State* state = new State();
+        state->getBoard().loadTerrainsFromFile("res/map.txt");
             
-            Unit* unit = new Leader(0, 1, 3, 2, RaceTypeId::HUMAN);
-            state->getBoard().addUnit(unit);
+        Unit* unit = new Leader(0, 1, 3, 2, RaceTypeId::HUMAN);
+        state->getBoard().addUnit(unit);
         
-            unit = new Leader(1, 2, 18, 6, RaceTypeId::ORC);
-            state->getBoard().addUnit(unit);
+        unit = new Leader(1, 2, 18, 6, RaceTypeId::ORC);
+        state->getBoard().addUnit(unit);
             
 
-            TerrainLayer* terrainLayer = new TerrainLayer(state->getBoard());
-            terrainLayer->initSurface();
-            
-            UnitLayer* unitLayer = new UnitLayer(state->getBoard());
-            unitLayer->initSurface();
-            
-            StateLayer* stateLayer = new StateLayer(*state);
-            stateLayer->initSurface();
+        TerrainLayer* terrainLayer = new TerrainLayer(state->getBoard());
+        UnitLayer* unitLayer = new UnitLayer(state->getBoard());
+        StateLayer* stateLayer = new StateLayer(*state);
+        
+        terrainLayer->initSurface();
 
-            while (window.isOpen())
-            {
-                sf::Event event;
-                while (window.pollEvent(event))
-                {
-                    if(event.type == sf::Event::Closed)
-                        window.close();
-                }
-
-                window.clear();
-                window.draw(*(terrainLayer->getSurface()));
-                window.draw(*(unitLayer->getSurface()));
-                window.draw(*(stateLayer->getSurface()));
-                window.display();
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if(event.type == sf::Event::Closed)
+                    window.close();
             }
+
+            unitLayer->initSurface();       
+            stateLayer->initSurface();
+            window.clear();
+            window.draw(*(terrainLayer->getSurface()));
+            window.draw(*(unitLayer->getSurface()));
+            window.draw(*(stateLayer->getSurface()));
+            window.display();
+        }
     }
 }
