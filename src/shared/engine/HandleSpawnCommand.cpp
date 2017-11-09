e615e06113bd850c21b0a99ec801f315b1b4e247
mathis <mathis.oudin@ensea.fr>
2017-11-09 17:15:39 +0100

    Implementation of HandleSpawnCommand

--- src/shared/engine/HandleSpawnCommand.cpp
+++ src/shared/engine/HandleSpawnCommand.cpp
@@ -3,8 +3,15 @@
 namespace engine {
 
     void HandleSpawnCommand::spawnUnit(state::State& state, int x, int y, state::UnitTypeId unitTypeId) {
-        if (state.getBoard().findTerrainOnPosition(x, y)->getTypeId() == state::TerrainTypeId::CASTLE && state.getBoard().findUnitOnPosition(x, y)->getTypeId() == state::UnitTypeId::LEADER) {
-            
+        if (state.getBoard().findTerrainOnPosition(x, y)->getTypeId() == state::TerrainTypeId::CASTLE && 
+                state.getBoard().findUnitOnPosition(x, y)->getTypeId() == state::UnitTypeId::LEADER) {
+            if (state.getBoard().isUnitOnPosition(x - 1, y - 1)) {
+                state.getBoard().addUnit(unitTypeId, state.getBoard().findTeam(state.getTurn()%2)->getRace(), x - 1, y - 1);
+            } else if (state.getBoard().isUnitOnPosition(x, y - 1)) {
+                state.getBoard().addUnit(unitTypeId, state.getBoard().findTeam(state.getTurn()%2)->getRace(), x, y - 1);
+            } else if (state.getBoard().isUnitOnPosition(x + 1, y - 1)) {
+                state.getBoard().addUnit(unitTypeId, state.getBoard().findTeam(state.getTurn()%2)->getRace(), x + 1, y - 1);
+            }
         }
     }
 
