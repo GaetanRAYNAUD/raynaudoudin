f01e31d84986c71944de1026dda2676700b788ee
Gaetan <gaetan.raynaud@ensea.fr>
2017-12-19 15:06:13 +0100

    mplement network test

---
+++ src/client/network.hpp
@@ -0,0 +1,16 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+#include "server.h"
+
+#include <iostream>
+#include <sstream>
+#include <string.h>
+#include <memory>
+
+namespace server {
+    void network_Test();    
+}
+
+#endif /* NETWORK_HPP */
+
