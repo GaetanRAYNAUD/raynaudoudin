10df9b4ea333a62aa6b84d2f195f06cc59c261bd
Gaetan <gaetanraynaud@outlook.com>
2018-01-11 23:59:37 +0100

    Update to 4.2

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -124,7 +124,7 @@ int main(int argc, char* argv[]) {
                 name = string(argv[3]);
             }
             
-            network_Test2(port, name);
+            network_Test(port, name);
             
         } else {
             listCommands();
